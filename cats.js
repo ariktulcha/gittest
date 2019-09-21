e162239e2b3c4c56c96d30acff86e91b02672397
ariktulcha <ariktulcha@gmail.com>
2019-09-21 09:43:14 +0000

    add var

--- cats.js
+++ cats.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 console.log("hello");
 console.log("Arik is the king");
 console.log("Arik is the king");
 console.log("Arik is the king");
-=======
->>>>>>> parent of bfbd7f7... new console.log
+
+var x = "arik";
