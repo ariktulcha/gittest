6f35a918d6c20f9ba8cb58cd0a73c8eaa0742cdc
EC2 Default User <ec2-user@ip-172-31-36-186.us-east-2.compute.internal>
2019-09-21 09:37:18 +0000

    Add new files to github

---
+++ cats.js
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+console.log("hello");
+console.log("Arik is the king");
+console.log("Arik is the king");
+console.log("Arik is the king");
+=======
+>>>>>>> parent of bfbd7f7... new console.log
---
+++ dogs.js
@@ -0,0 +1,2 @@
+console.log("Arik is the king");
+console.log("mosha is the king");
